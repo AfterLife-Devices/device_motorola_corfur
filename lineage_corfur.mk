bfddb89eee64e48db3b4412ad2aa9c80307ebb4d
KevinFrax <kevin.vm1511@gmail.com>
2023-12-10 22:11:14 +0000

    target bootanimation default by sozosha

--- lineage_corfur.mk
+++ lineage_corfur.mk
@@ -13,6 +13,7 @@ $(call inherit-product, device/motorola/corfur/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := lineage_corfur
 PRODUCT_DEVICE := corfur
