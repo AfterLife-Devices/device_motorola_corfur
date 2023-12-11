a123d4d0d5e0e8ac58d70615abf2dfa43474776b
KevinFrax <kevin.vm1511@gmail.com>
2023-12-11 01:26:30 +0000

    add vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/KevinFrax/vendor_motorola_corfur vendor/motorola/corfur
+
 git clone --depth=1 https://github.com/KevinFrax/android_kernel_motorola_sm6375 kernel/motorola/sm6375 
+
 git clone git clone https://github.com/LineageOS/android_hardware_motorola hardware/motorola
+
 git clone https://github.com/LineageOS/android_system_qcom system/qcom
